4640fca4c2a75a08dd58fb137f7a1c11bed354ac
Danil <25121341+LowSkillDeveloper@users.noreply.github.com>
2019-12-16 18:17:12 +0200

    Update autoWPS.sh

--- autoWPS.sh
+++ autoWPS.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 while (true)
 do
+#bash kill.sh
 killall wifite;
-wifite -i wlan0 -p 5 --wps --wps-only --pixie --wps-fails 1 --wps-timeouts 1 --wps-time 5;
+wifite -i wlan0 -p 5 --wps --wps-only --pixie --wps-fails 1 --wps-timeouts 1 --wps-time 15;
 done;
